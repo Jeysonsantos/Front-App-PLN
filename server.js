37067de26502f504bf57a375e69cc092a0c588df
Jeysonsantos <jeyson-santos@hotmail.com>
2022-10-23 11:57:27 -0300

    Atualizando front

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
 const express = require('express');
 const app = express();
 const appName = 'front-app';
-const outputPath = `${__dirname}/dist/${appName}/app`;
+const outputPath = `${__dirname}/dist/${appName}`;
 
 app.use(express.static(outputPath));
 app.get('/*',(req,res) => {
-    res.sendFile(`${outputPath}/novo-link.component.html`);
+    res.sendFile(`${outputPath}/index.html`);
 })
 
 app.listen(process.env.PORT);
